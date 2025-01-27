67c5e1c2332e9a80817e9d223a140ac7db4130a6
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-27 04:11:46 +0100

    nix init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A development environment for Laravel projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # PHP and Composer
+            php
+            phpPackages.composer
+          ];
+        };
+      }
+    );
+}
