5c1fabfcdfe69c06b9ce73fcd79f15a5dec35d33
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-30 07:08:35 +0100

    fix: default value for old value in form-textarea

--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -1,5 +1,5 @@
 @include('components.form._form-label')
-<textarea placeholder="{{$placeholder}}" name="{{$name}}" class="editor w-full p-1 rounded-lg border border-gray-200 @error($name) border-red-500 @enderror" rows="5">{{old($name,$value)}}</textarea>
+<textarea placeholder="{{$placeholder}}" name="{{$name}}" class="editor w-full p-1 rounded-lg border border-gray-200 @error($name) border-red-500 @enderror" rows="5">{{old($name,$value ?? '')}}</textarea>
 @include('components.form._form-error-handling')
 <br/><br/>
 
